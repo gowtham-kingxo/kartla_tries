673429190ac7517f70f37ffbe130e87d65f0eb27
Gowtham Genupur <gowtham.gourov@gmail.com>
2017-05-06 18:54:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/gowthamg/kartla_tries/Main2Activity.java
@@ -0,0 +1,35 @@
+package com.example.gowthamg.kartla_tries;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                Log.i("hiiii", "Place: " + place.getName());
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i("hii", "An error occurred: " + status);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/gowthamg/kartla_tries/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.gowthamg.kartla_tries;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView image;
+
+    public  void downloadimage(View view)
+    {
+        DownloadTask task = new DownloadTask();
+        try {
+            Bitmap showimage = task.execute("https://kart.la/wp-content/uploads/2016/07/wp-image-1630888628jpeg-150x150.jpeg").get();
+            image.setImageBitmap(showimage);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void,Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection connection = null;
+
+
+            try {
+                url = new URL(urls[0]);
+                connection = (HttpURLConnection) url.openConnection();
+               connection.connect();
+                InputStream is = connection.getInputStream();
+                Bitmap mybitmap = BitmapFactory.decodeStream(is);
+                return mybitmap;
+
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+
+            }
+
+           return null;
+        }
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       // image = (ImageView) findViewById(R.id.imageView2);
+
+
+        DownloadTask task = new DownloadTask();
+
+
+    }
+}
